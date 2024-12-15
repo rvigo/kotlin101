ef2500f1f0442eaca63038817bdfde0761c890f4
Rafael Vigo <rvigo07+github@gmail.com>
2024-12-15 00:54:55 -0300

    build: includes kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
+import kotlinx.kover.gradle.plugin.dsl.AggregationType
+import kotlinx.kover.gradle.plugin.dsl.CoverageUnit
+import kotlinx.kover.gradle.plugin.dsl.GroupingEntityType
+
 plugins {
+    id("org.jetbrains.kotlinx.kover") version "0.9.0"
     kotlin("jvm") version "2.0.21"
 }
 
@@ -9,9 +14,11 @@ repositories {
     mavenCentral()
 }
 
+val junitVersion by extra { "5.11.3" }
+
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
 }
 
 tasks.test {
@@ -20,4 +27,29 @@ tasks.test {
 
 kotlin {
     jvmToolchain(21)
+}
+
+kover {
+    reports {
+        group = GroupingEntityType.CLASS
+
+        filters {
+            excludes {
+                classes(
+                    "**\$Companion",
+                    "**\$DefaultImpls",
+                    "**\$special**"
+                )
+            }
+        }
+        verify {
+            rule {
+                bound {
+                    coverageUnits = CoverageUnit.BRANCH
+                    minValue = 100
+                    aggregationForGroup = AggregationType.COVERED_PERCENTAGE
+                }
+            }
+        }
+    }
 }
